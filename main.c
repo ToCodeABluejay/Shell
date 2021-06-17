de8cf3b40d92d902c2c40cdecdd8300815fa9693
ToCodeABluejay <73219116+ToCodeABluejay@users.noreply.github.com>
2021-06-17 16:23:39 -0400

    Added 'NULL' parameter to second part of pledge()

--- main.c
+++ main.c
@@ -25,7 +25,7 @@
 int main(int argc, char *argv[])
 {
 	#ifdef __OpenBSD__
-	pledge("stdio rpath proc exec");
+	pledge("stdio rpath proc exec", NULL);
 	#endif
 
 	isnotsu = geteuid(); // Check if the user is a super-user or not
